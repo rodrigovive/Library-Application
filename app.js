ba334f7d7ec09791db4b6a79b56254527a5165a6
Rodrigo <cherry199662@gmail.com>
2018-09-17 01:54:29 -0500

    Adding Eslint To Project

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use(morgan('tiny'));
 
+app.use('/css',
+    express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js',
+    express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js',
+    express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'/views/','/index.html') );
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
 app.listen(3000, function() {
