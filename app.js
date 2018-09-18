a5c24fa5a4576aa4027f1591a6369fcfde31a51c
Rodrigo <cherry199662@gmail.com>
2018-09-18 09:19:47 -0500

    Integrating ejs and StoreStrap Bootstrap

--- app.js
+++ app.js
@@ -16,11 +16,15 @@ app.use('/js',
   express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js',
   express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+  // res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+  res.render('index', { title: 'Aplication for weather' });
 });
 
 app.listen(port, () => {
-  debug(`listening on node port ${chalk.green('3000')}`);
+  debug(`listening on node port ${chalk.green(port)}`);
 });
